4ab01997aaf0f0a2e02910607a979a5c8300107b
daviengush@gmail.com <daviengush@gmail.com>
2023-01-25 12:45:35 +0000

    Write a Python program to construct the following pattern, using a nested for loop.
    *
    
    * *
    
    * * *
    
    * * * *
    
    * * * * *
    
    * * * *
    
    * * *
    
    * *
    
    *

--- test/star.py
+++ test/star.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
 
-#prints a pattern
+#store the value
+n = 5
+
+#print * starting with the first one
+for i in range(n):
+    for j in range(i):
+         print('*', end="")
+#print a new line
+    print()
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('*', end="")
+
+#print a new line
+    print()
+    
+    
+
