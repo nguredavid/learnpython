31af6640af4461c1ba5c51410d84a1a153e49a2c
daviengush@gmail.com <daviengush@gmail.com>
2023-01-25 16:34:30 +0000

    combination of numbers

---
+++ test/invent.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for i in range(0,9):
+    for j in range(1,10):
+
+        print(f'{i}{j},',end='')
+       
+print()
--- test/star.py
+++ test/star.py
@@ -3,19 +3,14 @@
 #store the value
 n = 5
 
-#print * starting with the first one
+#print the pattern correctly
+
 for i in range(n):
     for j in range(i):
-         print('*', end="")
-#print a new line
+        print('*', end="")
     print()
 
 for i in range(n,0,-1):
     for j in range(i):
         print('*', end="")
-
-#print a new line
-    print()
-    
-    
-
+    print()   
