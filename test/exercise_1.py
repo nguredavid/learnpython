d2eac749ba468d54653f30adc346de14694028e0
daviengush@gmail.com <daviengush@gmail.com>
2023-01-25 12:53:58 +0000

    Exercise 1: Write question words 3 times using nested loops

---
+++ test/exercise_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#declare valuables
+count = [1,2,3]
+words = ['when', 'what','why']
+
+#put conditions to print the outerloop
+for i in count:
+    print(i)
+    
+#print the outerloop
+    for j in words:
+        print(j)
+
