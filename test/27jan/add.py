ed9009f1a45cef407b4ee6e5cea01bf90c8b16e0
daviengush@gmail.com <daviengush@gmail.com>
2023-01-28 08:04:38 +0000

    update

--- test/27jan/add.py
+++ test/27jan/add.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 
-#using function to add two numbers
+#using function to add two number
 
-def add(num1, num2):
-    result = num1 + num2
+def add(a, b):
+    result = a + b
     return result
 
-#prompt user to enter 2 numbers and call fxn for sum
-
-sum = int(input('Please the first numbers '))
-sum2 = int(input('Please enter any two numbers '))
-result = add(sum, sum2)
-print("The sum of the two numbers is", result)
+num1 = 20
+num2 = 40
+result = add(num1, num2)
+print('The sum of the two numbers is :', result)
