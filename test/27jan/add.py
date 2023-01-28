d39f9ab6603632b51bda772b1d643c15b7b45abc
daviengush@gmail.com <daviengush@gmail.com>
2023-01-28 07:48:39 +0000

    lost

--- test/27jan/add.py
+++ test/27jan/add.py
@@ -4,10 +4,11 @@
 
 def add(num1, num2):
     result = num1 + num2
-    print('The sum of the two numbers is: ', result)
+    return result
 
 #prompt user to enter 2 numbers and call fxn for sum
 
 sum = int(input('Please the first numbers '))
 sum2 = int(input('Please enter any two numbers '))
-add(sum, sum2)
+result = add(sum, sum2)
+print("The sum of the two numbers is", result)
