6b3953d215fd573a5ea437e75f4f91dda306c13e
daviengush@gmail.com <daviengush@gmail.com>
2023-01-30 15:15:44 +0000

    learing abt functions

---
+++ test/30jan/exe1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#function that accepts name and age
+
+def accept():
+    name = str(input('Please enter your name: '))
+    age = int(input('please enter your age: '))
+    print(f'your name is {name} {age} years of age')
+
+accept()
