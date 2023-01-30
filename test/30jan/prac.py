58fd8c247b147e64f1f55f1c813353bde96ae7dd
daviengush@gmail.com <daviengush@gmail.com>
2023-01-30 04:07:11 +0000

    funtion to multiply and add 2 numbers

---
+++ test/30jan/prac.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+#function to add
+
+def add_numbers(num1, num2):
+    result = num1 + num2
+    return result
+
+#fxn to multiply
+def multiply_numbers(nr1, nr2):
+    result = nr1 * nr2
+    return result
+
+numb1 = 39
+numb2 = 40
+result = add_numbers(numb1, numb2)
+print('The sum of two numbers is  ', result)
+
+result = multiply_numbers(numb1, numb2)
+print('The mutliplication is  ', result)
