c7be18728f7f21d2bf6f7934571cf91ec7f8f3ce
daviengush@gmail.com <daviengush@gmail.com>
2023-02-08 03:36:16 +0000

    still learing guys

--- test/6feb/school.py
+++ test/6feb/school.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+
+#create a class
+
+class Triangle:
+    def __init__(self, a, b, c):
+
+        #instance variables
+        self.w = a
+        self.b = b
+        self.c = c
+        
+        #instance method
+
+    def calc(self):
+        perimeter = self.w + self.b + self.c
+        return perimeter
+    #define obects
+
+t1 = Triangle(4, 7, 4)
+ 
+ #call methods
+
+perimeter = t1.calc()
+
+print("", perimeter)
