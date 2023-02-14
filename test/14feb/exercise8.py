04467973fbdfdc4ebd691b0739143000e27a69ed
daviengush@gmail.com <daviengush@gmail.com>
2023-02-14 03:03:53 +0000

    Exercise 8: Print list in reverse order using a loop

--- test/14feb/exercise8.py
+++ test/14feb/exercise8.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 #Exercise 8: Print list in reverse order using a loop
+list = [10, 20, 30, 40, 50]
 
-list1 = [10, 20, 30, 40, 50]
-
-new_list = reversed(list1)
-
+new_list = reversed(list)
 for i in new_list:
     print(i)
+
