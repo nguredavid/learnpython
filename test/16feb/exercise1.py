47f6b2069d1cccff726439e9d1003939d02be021
daviengush@gmail.com <daviengush@gmail.com>
2023-02-16 15:41:54 +0000

    exer1

---
+++ test/16feb/exercise1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#calc multip and sum of two numbers
+
+num1 = int(input('Enter the first number'))
+num2 = int(input('Enter the second number'))
+product = num1*num2
+sum = num1+num2
+
+if (product <= 1000):
+    
+    print(product)
+else:
+    print(sum)
