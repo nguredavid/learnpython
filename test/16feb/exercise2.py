f07af1556de6c1603b87789c2bd7ade8b29e26ef
daviengush@gmail.com <daviengush@gmail.com>
2023-02-16 16:46:04 +0000

    Yeah, am just okay

--- test/16feb/exercise2.py
+++ test/16feb/exercise2.py
@@ -3,5 +3,6 @@
 #okay, test your muslce David
 for i in range(1, 11):
     previous_n = i-1
-    print(i+previous_n)
+    summ = i+previous_n
+    print(f'Current number is {i} and the previous number is {previous_n} sum is {summ}')
     
