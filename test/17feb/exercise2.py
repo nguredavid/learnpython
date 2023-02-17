ab5a46092db8d00ec0786c9233f577071d28c463
daviengush@gmail.com <daviengush@gmail.com>
2023-02-17 04:10:56 +0000

    learn on sep="@@"

---
+++ test/17feb/exercise2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+#The string to be separate by for instance special characters
+
+name = str(input('Please enter the names to separate'))
+
+#print output
+
+print(name, sep='@@')
+print('it', 'is', 'correct', sep='@@')
+
