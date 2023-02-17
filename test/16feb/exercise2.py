0ec5c0015874b3c5410e8228a2a6184ae2cd8f69
daviengush@gmail.com <daviengush@gmail.com>
2023-02-17 03:52:04 +0000

    prompted the user to input a number

--- test/16feb/exercise2.py
+++ test/16feb/exercise2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 #okay, test your muslce David
-for i in range(1, 11):
+num = int(input('Please enter a number'))
+for i in range(1, num):
     previous_n = i-1
     summ = i+previous_n
     print(f'Current number is {i} and the previous number is {previous_n} sum is {summ}')
