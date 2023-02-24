b0b2ac42762865c51df4efae7323d62ec6ca01be
daviengush@gmail.com <daviengush@gmail.com>
2023-02-24 05:34:45 +0000

    update feature

--- test/24feb/calc.py
+++ test/24feb/calc.py
@@ -6,16 +6,16 @@ num1 = float(input("Please enter the 1st number "))
 num2 = float(input("Please enter the 2nd number "))
 def add():
     result = num1 + num2
-    print(result)
+    print(f'{num1} + {num2} = {result}')
 def mult():
     result = num1 * num2
-    print(result)
+    print(f'{num1} * {num2} = {result}')
 def div():
     result = num1 / num2
-    print(result)
+    print(f'{num1} / {num2} = {result}')
 def subt():
     result = num1 - num2
-    print(result)
+    print(f'{num1} - {num2} = {result}')
 
 operator = input("Please choose your operator as either (-, /, +, *):")
 
