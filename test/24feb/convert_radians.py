30f552b1fce79723dd4d755f0ac4ff1c78d3c83d
daviengush@gmail.com <daviengush@gmail.com>
2023-02-24 09:21:36 +0000

    1. Convert radians into degrees

---
+++ test/24feb/convert_radians.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#1. Convert radians into degrees
+
+def convert():
+    constant = 57.2958
+    degree = radians * (180 / 3.143)
+    print(f'{radians:03} is equivalent to  {degree:.2f} degrees')
+
+radians = float(input('Please enter radians to convert: '))
+convert()
+
+    
+
