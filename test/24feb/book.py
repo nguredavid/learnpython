dbfcf9f760e72b9fbf526016505069c2099d1fc9
daviengush@gmail.com <daviengush@gmail.com>
2023-02-24 07:52:47 +0000

    wow, just solved it

---
+++ test/24feb/book.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#Question 3: My Favorite Book
+
+
+def favorite_book(title, author):
+    pass
+
+b_title = "The History of Bhutan"
+b_author = "Karma Phuntsho"
+
+book = favorite_book(b_title, b_author)
+
+print(f'{b_title} by {b_author}')
+
+
