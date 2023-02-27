49a99443c5e52fffdb409e8cbd24e2e4a12eee07
daviengush@gmail.com <daviengush@gmail.com>
2023-02-27 15:53:35 +0000

    second problem on module solved

---
+++ Python_imports_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+#import the function calc_1.py
+
+import calc_1
+
+#define variables
+
+a = 5 
+b = 10
+
+result = calc_1.subt(a, b)
+
+print(f'{a} - {b} = {result}')
+
+sums = calc_1.mult(a, b)
+
+print(f'{a} - {b} = {sums}')
---
+++ Python_imports_modules/calc_1.py
@@ -0,0 +1,15 @@
+#This function should only be imported and not executed
+#addition fxn
+
+def sum(a, b):
+    return a + b
+
+#mult fxn
+
+def mult(a, b):
+    return a * b
+
+#subt fxn
+def subt(a, b):
+    return a - b
+
