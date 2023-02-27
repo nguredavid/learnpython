92851da91ac8ec0c4e02dddb6f8d32fabd9ad0f0
daviengush@gmail.com <daviengush@gmail.com>
2023-02-27 15:30:05 +0000

    module 2 pratice

---
+++ Python_imports_modules/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#import the initially created function
+
+import add_0
+a = 10
+b = 23
+result = add_0.add(a, b)
+print(result)
+
---
+++ Python_imports_modules/add_0.py
@@ -0,0 +1,5 @@
+#So technically this fxn in this module needs to be imported
+
+def add(a, b):
+    return a + b
+
