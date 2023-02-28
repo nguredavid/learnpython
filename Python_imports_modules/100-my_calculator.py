7eddf499b313e79d09a4dd41521c97131f44b258
daviengush@gmail.com <daviengush@gmail.com>
2023-02-28 03:43:07 +0000

    a few modifications on calculator done

--- Python_imports_modules/100-my_calculator.py
+++ Python_imports_modules/100-my_calculator.py
@@ -7,27 +7,35 @@ import calc_1
 num1 = 10
 num2 = 30
 
-operator = input('Please selector operator *, /, +, - : \n')  
 
-if operator == "*":
-    mult = calc_1.mult(num1, num2)
-    print(f'{num1} * {num2} = {mult}')
+while True:
+    operator = input('Please selector operator *, /, +, - : \n')
+    if operator in ("*", "+", "-", "+"):
+         
+        if operator == "*":
+            mult = calc_1.mult(num1, num2)
+            print(f'{num1} * {num2} = {mult}')
 
-elif operator == "+":
-    sums = calc_1.sum(num1, num2)
-    print(f'{num1} + {num2} = {sums}')
+        elif operator == "+":
+            sums = calc_1.sum(num1, num2)
+            print(f'{num1} + {num2} = {sums}')
 
-elif operator == "/":
-    divs = calc_1.div(num1, num2)
-    print(f'{num1} / {num2} = {divs}')
+        elif operator == "/":
+            divs = calc_1.div(num1, num2)
+            print(f'{num1} / {num2} = {divs}')
 
-elif operator == "-":
-    sub = calc_1.mult(num1, num2)
-    print(f'{num1} - {num2} = {sub}')
-    
-else:
+        elif operator == "-":
+            sub = calc_1.mult(num1, num2)
+            print(f'{num1} - {num2} = {sub}')
+        next_calc = input('Do you want another calculation Y/N')
+        if next_calc == "N":
+            break
+            
 
-    print('You have entered invalid function')
+    else:
+        print('You have entered invalid function')
+        
+     
     
 
 
