1445fef690b99ba9312f46da55e2e4a5269b9c79
daviengush@gmail.com <daviengush@gmail.com>
2023-03-02 04:32:04 +0000

    checking if the item is in the list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,5 +4,10 @@
 
 languages = ['python', 'c++', 'c', 'java']
 
-print('c' in languages)
-print('c+' in languages)
+#print('c' in languages)
+#print('c+' in languages)
+i = str(input('please enter value to check :'))
+if i in languages:
+    print('in the list')
+else:
+    print('not in the list')
