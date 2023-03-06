2626c5ecb0db619aa107473fe7d137e73048d8af
daviengush@gmail.com <daviengush@gmail.com>
2023-03-06 03:34:46 +0000

    just open the code and see it

--- python-data_structures/exercise3.py
+++ python-data_structures/exercise3.py
@@ -3,12 +3,16 @@
 #Slice list into 3 equal chunks and reverse each chunk
 
 sample_list = [11, 45, 8, 23, 14, 12, 7, 45, 89]
+print('Initial list ', sample_list)
+
+length_size = len(sample_list)
+chunk_size = length_size / 3
 
 
+print(length_size)
 sample_list.reverse()
 
 print('After reversing ', sample_list)
-print('Initial list ', sample_list)
 
 
 
