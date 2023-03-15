64c084d80b7031b6d7bb14897525b08e3a940f5f
daviengush@gmail.com <daviengush@gmail.com>
2023-03-15 15:24:54 +0000

    Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.

---
+++ python-data_structures/book.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.
+
+books = ("Through my african eyes", "culture map", "book3", "book4", "book5")
+
+for char in books:
+    print(char)
+
