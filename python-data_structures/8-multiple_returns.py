dd7a38f8142552ccda1cf3539d5ad56e69458c50
daviengush@gmail.com <daviengush@gmail.com>
2023-03-15 16:05:33 +0000

    Write a function that returns a tuple with the length of a string and its first character.

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+    return sentence
+
+sen = ("David Ngure Ngonyo")
+
+print(f'The length of your string is :  {len(sen)} and the first character is  {sen[0]}')
+multiple_returns(sen)
