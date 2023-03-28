cb2aacec82522b455c63d2eb456fc7de841d4c50
daviengush@gmail.com <daviengush@gmail.com>
2023-03-28 04:36:47 +0000

    update

---
+++ Exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    for i in lists:
+        print(i, end="")
+    print()
+    
+
+
+lists = [1, 2, 3, 4, 5]
+
+elements = 5
+
+
+safe_print_list(lists, elements)
