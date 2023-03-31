76476074c2207fec7c1fb74aa6b4c17771f18901
daviengush@gmail.com <daviengush@gmail.com>
2023-03-31 02:33:09 +0000

    update on error handling

--- Exceptions/example.py
+++ Exceptions/example.py
@@ -4,7 +4,10 @@ while True:
     
     try:
         num = int(input('Enter a number'))
+        print(f'You have entered {num}')
+        
 
         break
     except ValueError:
         print('Oops! That was an invalid number, please try again')
+    
