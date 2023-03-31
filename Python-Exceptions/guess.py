14d3eedc8f7bd040c1a1b676acd5efb25b22ddf4
daviengush@gmail.com <daviengush@gmail.com>
2023-03-31 02:17:49 +0000

    exceptions

--- Python-Exceptions/guess.py
+++ Python-Exceptions/guess.py
@@ -5,8 +5,17 @@
 secret_number = 5
 
 while True:
-    guess = int(input('Enter a number between 1 and 5'))
+    try:
+        guess = int(input('Enter a number between 1 and 5: '))
             
-    if guess == secret_number:
-                print('Your guess is right')
-                break
+        if guess != secret_number:
+            print("Sorry, please try again")
+            
+        else:
+            print('Your guess is right')
+            break
+
+        
+           
+    except ValueError:
+            print("Invalid, please enter a number")
