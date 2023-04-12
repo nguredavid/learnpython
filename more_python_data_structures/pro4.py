20d9e085ec2c18421417a697e70fac5325f2b6e9
daviengush@gmail.com <daviengush@gmail.com>
2023-04-12 14:07:30 +0000

    printing a set

---
+++ more_python_data_structures/pro4.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+#define fxn
+
+def duplicateEliminator(sets):
+
+    for i in sets:
+        print(i)
+
+s = {1, 1, 1, 2, 3, 5, 3}
+print(s)
+
+res = duplicateEliminator(s)
+
+
