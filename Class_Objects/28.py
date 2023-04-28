5c76975ec7e9d523f71fafce862155ec20d23f47
daviengush@gmail.com <daviengush@gmail.com>
2023-04-28 07:46:06 +0000

    update

---
+++ Class_Objects/28.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+'''Doc String'''
+
+'''create class'''
+class Bus:
+    def __init__(self, mileage, name, max_speed):
+
+        '''members of the object'''
+
+        self.name = name
+        self.mileage = name
+        self.max_speed = max_speed
+    def seating_capacity(self, capacity):
+        '''self.capacity = 50'''
+
+        print(f'The seating capacity of a {self.name} is {capacity} passangers')
+'''create object'''
+vehicle = Bus(10000, 'Audi', 150)
+vehicle.seating_capacity(50)
+
+
+
