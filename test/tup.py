e631a169fd4b95fa74f7e1f55e16c216e92d72cb
daviengush@gmail.com <daviengush@gmail.com>
2023-06-21 16:37:15 +0000

    tuple

---
+++ test/tup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+'''Create a tuple containing the names of five of your favorite books. Then, use a for loop to print each book name on a separate line.'''
+
+books = ("Through my African Eyes", "Culure Map", "Utengano", "Kifo Kisimani", "Half a Day")
+
+for book in books:
+    print(book)
+    print()
