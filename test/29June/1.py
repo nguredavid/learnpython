2c350c1d54368c1fe9c35266d5ae9dfa52733f7e
daviengush@gmail.com <daviengush@gmail.com>
2023-06-29 16:13:40 +0000

    exercise 1

---
+++ test/29June/1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+'''Write a Python program to create a class representing a Circle. Include methods to calculate its area and perimeter.'''
+
+'''create a class called circle'''
+pie = 3.14
+
+class Circle:
+
+
+
+    '''class variable'''
+    
+
+    '''create a constructor or the initailize method'''
+
+    def __init__ (self, radius):
+
+        self.radius = radius
+
+    '''method to calculate area'''
+    def area(self):
+        
+       self.areas = pie*self.radius**2
+
+    '''method to calculate perimeter'''
+
+    def perime(self):
+
+        self.perimeter = 4*pie*self.radius
+
+    '''print area and perimeter'''
+    def display(self):
+
+        print(f'The area of your circle is : {self.areas} units squared')
+        print(f'The perimeter of your circle is : {self.perimeter} units squared')
+
+'''create object'''
+
+circle = Circle(7)
+
+circle.area()
+circle.perime()
+circle.display()
