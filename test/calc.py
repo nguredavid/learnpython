0a15f3011ccbc0d0aef3e50143478a33254056d9
daviengush@gmail.com <daviengush@gmail.com>
2023-07-11 15:36:47 +0000

    changes made

--- test/calc.py
+++ test/calc.py
@@ -5,34 +5,37 @@ class Calculator:
 
     '''initialize the class'''
     def __init__(self, num1, num2):
-
-        self.num1 = num1
+        self.num1 = num2
         self.num2 = num2
 
     '''method for addition'''
     def adds(self):
 
-        return self.num1 + self.num2
+        return self.num1+self.num2
 
     '''method for ubstraction'''
     def sub(self):
 
-        return self.num1 - self.num2
+        return self.num1-self.num2
 
     '''method for multiplication'''
     def mult(self):
 
-        return self.num1 * self.num2
+        return self.num1*self.num2
 
     '''method for division'''
 
     def div(self):
 
-        return self.num1 / self.num2
+        return self.num1/self.num2
 
-'''create  the object'''
-calc = Calculator(30, 10)
-while True:
+'''prompt user to enter numbers'''
+um1 = int(input("Please enter your first number "))
+um2 = int(input("Please enter your second number "))
+print(f'Dear user, you have entered the following numbers: {um1} and {um2}')
+
+calc = Calculator(um1, um2)
+while True:   
 
     result = input("Please choose the arithemtic to perform by entering +, -, *, / or q to quit: ")
     if result == "q":
