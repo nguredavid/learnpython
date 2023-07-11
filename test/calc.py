72560df14b252026a30d78e97a9dec26283e62d3
daviengush@gmail.com <daviengush@gmail.com>
2023-07-11 15:53:52 +0000

    error handling added

--- test/calc.py
+++ test/calc.py
@@ -5,7 +5,7 @@ class Calculator:
 
     '''initialize the class'''
     def __init__(self, num1, num2):
-        self.num1 = num2
+        self.num1 = num1
         self.num2 = num2
 
     '''method for addition'''
@@ -30,8 +30,16 @@ class Calculator:
         return self.num1/self.num2
 
 '''prompt user to enter numbers'''
-um1 = int(input("Please enter your first number "))
-um2 = int(input("Please enter your second number "))
+while True:
+    try:
+        um1 = int(input("Please enter your first number "))
+        um2 = int(input("Please enter your second number "))
+        break
+    except ValueError:
+
+        print("Invalid input, please enter an integer.")
+
+
 print(f'Dear user, you have entered the following numbers: {um1} and {um2}')
 
 calc = Calculator(um1, um2)
