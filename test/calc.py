3623e435a116c2e09d31ee1286fff25c13530806
daviengush@gmail.com <daviengush@gmail.com>
2023-07-11 14:09:54 +0000

    out of boredom, simple calc coded

---
+++ test/calc.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+'''3. Write a Python program to create a calculator class. Include methods for basic arithmetic operations.'''
+'''create a class called calculator'''
+class Calculator:
+
+    '''initialize the class'''
+    def __init__(self, num1, num2):
+
+        self.num1 = num1
+        self.num2 = num2
+
+    '''method for addition'''
+    def adds(self):
+
+        return self.num1 + self.num2
+
+    '''method for ubstraction'''
+    def sub(self):
+
+        return self.num1 - self.num2
+
+    '''method for multiplication'''
+    def mult(self):
+
+        return self.num1 * self.num2
+
+    '''method for division'''
+
+    def div(self):
+
+        return self.num1 / self.num2
+
+'''create  the object'''
+calc = Calculator(30, 10)
+result = input("Please choose the arithemtic to perform by entering +, -, *, / : ")
+if result == "+":
+    print(calc.adds())
+elif result == "-":
+    print(calc.sub())
+elif result == "*":
+    print(calc.mult())
+elif result == "/":
+    print(calc.div())
+else:
+    print("Sorry, You have entered a wrong operator, please try again")
+
+
