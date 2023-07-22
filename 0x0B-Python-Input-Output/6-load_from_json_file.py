50da24a45813829136920b530f645fc82765f9f0
David <64853005+nguredavid@users.noreply.github.com>
2023-07-22 12:08:25 +0300

    Update 6-load_from_json_file.py
    recover attempt

--- 0x0B-Python-Input-Output/6-load_from_json_file.py
+++ 0x0B-Python-Input-Output/6-load_from_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-'''Write a function that creates an Object from a “JSON file”:  '''
+'''Write a function that creates an Object from a “JSON file” :    '''
 import json
 def load_from_json_file(filename):
 
