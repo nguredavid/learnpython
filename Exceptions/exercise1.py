e5690a7bc344569de1f2113eb9887bbc118410bd
daviengush@gmail.com <daviengush@gmail.com>
2023-08-01 05:08:39 +0000

    while loop

--- Exceptions/exercise1.py
+++ Exceptions/exercise1.py
@@ -4,19 +4,22 @@
 
 def division():
     while True:
-        num = input("Please enter 2 numbers: ")
-
+    
         try:
-            num1, num2 = num.split()
+            num = input("Please enter 2 numbers: ")
+            num1, num2 = num.split("/")
             num1 = int(num1)
             num2 = int(num2)
             result = num1 / num2
+
         except ValueError:
             print("Please input integer")
         except ZeroDivisionError:
             print("number cannot be divided by zero")
+        else:
             break
-        print(f"{num1} / {num2} = {result}")
+    print(f"{num1} / {num2} = {result}")
+            
 
 output = division()
 #print(output)
