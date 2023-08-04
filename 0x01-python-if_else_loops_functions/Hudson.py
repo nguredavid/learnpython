787011273c311e59c9004e084fe51dbe4955119e
daviengush@gmail.com <daviengush@gmail.com>
2023-08-04 14:48:39 +0000

    update

---
+++ 0x01-python-if_else_loops_functions/Hudson.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def split_sentence():
+    sentence = input("Enter a sentence: ")
+    words = sentence.split()
+    print(words)
+
+    print("Individual words:")
+   # for word in words:
+        #print(word)
+
+split_sentence()
+
