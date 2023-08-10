0e32c697fc57cea61eb7683afb3cb913c3970204
daviengush@gmail.com <daviengush@gmail.com>
2023-08-10 07:48:42 +0000

    auxilliray fxns

---
+++ test/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def main():
+    num = int(input("Enter the number to square: "))
+    print("num squared is", add(num))
+    
+def squar(a):
+
+    return a**2
+def add(a):
+    return a*2
+main()
