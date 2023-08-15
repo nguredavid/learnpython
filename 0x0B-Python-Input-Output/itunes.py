b24c0b37f4bb89f8af84620b02186b051609335a
daviengush@gmail.com <daviengush@gmail.com>
2023-08-15 15:18:44 +0000

    itunes

--- 0x0B-Python-Input-Output/itunes.py
+++ 0x0B-Python-Input-Output/itunes.py
@@ -6,5 +6,10 @@ import requests
 if len(sys.argv) != 2:
     sys.exit()
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2)
-        
+#print(json.dumps(response.json(), indent=2)
+data = response.json()
+
+for resul in data["results"]:
+     print(resul["trackName"])
+
+
