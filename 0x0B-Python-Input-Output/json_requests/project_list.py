774596d3c55e3e882e2c5a90eefab2ad5685759e
daviengush@gmail.com <daviengush@gmail.com>
2023-08-16 15:59:17 +0000

    call the endpoint and print the name of the SECOND project on the list:

--- 0x0B-Python-Input-Output/json_requests/project_list.py
+++ 0x0B-Python-Input-Output/json_requests/project_list.py
@@ -8,9 +8,9 @@ import requests
 response = requests.get("https://assets.breatheco.de/apis/fake/sample/project_list.php")
 
 p_data = response.json()
-for project in p_data:
-    print(project["name"])
-#print(p_data["name"])
+#for project in p_data[1]:
+#print(project)
+print(p_data[1]["name"])
 #readable_data = json.dumps(p_data, indent=2, sort_keys=False)
 #print(readable_data)
 #print(len(readable_data))
