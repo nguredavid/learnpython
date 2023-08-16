885b70f83c9e02e527d08b7b0b73e432397ddd67
daviengush@gmail.com <daviengush@gmail.com>
2023-08-16 05:37:16 +0000

    access names

--- 0x0B-Python-Input-Output/example1.py
+++ 0x0B-Python-Input-Output/example1.py
@@ -6,5 +6,11 @@ import json
 response = requests.get("http://api.open-notify.org/astros.json")
 
 py_data = response.json()
-read = json.dumps(py_data, indent=2, sort_keys=True)
-print(read)
+#read = json.dumps(py_data, indent=2, sort_keys=True)
+#print(read)
+
+access = py_data["people"]
+
+for person in access:
+    print(person["name"])
+#print(access)
